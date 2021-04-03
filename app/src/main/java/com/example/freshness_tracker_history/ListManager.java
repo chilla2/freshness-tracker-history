e7382126a9b1db78d2c0a0ed714fda0686ed68ce
April <64.aprilhill@gmail.com>
2021-04-02 23:00:22 -0600

    Added create food item method to write to create JSON object.

---
+++ app/src/main/java/com/example/freshness_tracker_history/ListManager.java
@@ -0,0 +1,4 @@
+package com.example.freshness_tracker_history;
+
+public class ListManager {
+}
---
+++ app/src/main/java/com/example/freshness_tracker_history/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.freshness_tracker_history;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    ListManager mainInventory;
+    ArrayAdapter<String> arrayAdapter;
+    private static final String FILE_NAME = "food-item-list";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void onAddFoodItem(View view){
+        //
+    }
+    private void onSearch(View view){
+
+    }
+
+    private void createFoodItem(String name, Integer date, String category) {
+
+        JSONObject foodItem = new JSONObject();
+        try {
+            foodItem.put("Name", name);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            foodItem.put("Expiration Date", date);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            foodItem.put("Category", category);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        // Convert JsonObject to String Format
+        String foodItemString = foodItem.toString();
+        // Define the File Path and its Name
+        File file = new File(this.getFilesDir(),FILE_NAME);
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        try {
+            bufferedWriter.write(foodItemString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
